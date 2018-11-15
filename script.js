69946cee42a0547bf366884f43d3ca6d8f8fccf0
taniyaamidon <harshanitaniya@yahoo.co.uk>
2018-11-15 14:14:48 +0000

    JS added

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function calculateTip() {
     var billAmt = document.getElementById("billAmt").value;
     var serviceQual = document.getElementById("serviceQual").value;
     var peopleAmt = document.getElementById("peopleAmt").value;
-}
 
 if (billAmt === "" || serviceQual == 0) {
     alert("Please enter values");
@@ -11,4 +10,23 @@ if (billAmt === "" || serviceQual == 0) {
 
 if (peopleAmt === "" || peopleAmt <=1) {
     peopleAmt = 1;
+    document.getElementById("each").style.display = "none";   
+} else {
+    document.getElementById("each").style.display = "block";
 }
+
+var total = Math.round(billAmt * serviceQual) / peopleAmt;
+
+total = total.toFixed(2);
+
+document.getElementById("totalTip").style.display = "block";
+document.getElementById("tip").innerHTML = total;
+
+}
+
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+document.getElementById("calculate").onclick =  function() {
+        calculateTip();   
+    };
